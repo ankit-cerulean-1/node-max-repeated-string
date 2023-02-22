c4d1fe8bf50dd7bb8e79733efd88dcdf1c3e7247
ankit-palli <palliankit123@gmail.com>
2023-02-22 18:46:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-max-repeated-string
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-exbqj8)
---
+++ index.js
@@ -0,0 +1,30 @@
+const app = require('express')();
+
+/**
+ * Problem Statement: Given list of items in array find maximum repeated item and display it on output.
+ * Input: List of Elements
+ * Output: Most Repeated String in Array
+ */
+var getLargestString = (req, res) => {
+  let input = [
+      'fan',
+      'table',
+      'computer',
+      'books',
+      'table',
+      'computer',
+      'table',
+    ],
+    output = 'Display String with Maximum repeated count in Array';
+
+  /** Write down your code here */
+
+  /** Logic Ends */
+  res.send('<h3>' + output + '</h3>');
+};
+
+app.get('/', getLargestString);
+
+app.listen(3000, () => {
+  console.log('Started listening');
+});
